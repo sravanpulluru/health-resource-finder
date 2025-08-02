ee3f56fec7b27b4793ec6763b71680c7ca97a951
venkata sravan pulluru <venkatasravan_pulluru@srmap.edu.in>
2025-08-02 09:05:17 +0530

    Update donorModel.js

--- backend/models/donorModel.js
+++ backend/models/donorModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const donorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  bloodGroup: { type: String, required: true },
-  location: { type: String, required: true },
-  contact: { type: String, required: true },
-  available: { type: Boolean, default: true }
-}, { timestamps: true });
+  name: String,
+  bloodGroup: String,
+  location: String,
+  contact: String
+});
 
 module.exports = mongoose.model('Donor', donorSchema);
